5385717f772e0d570cc061f44d3bdd2ae2e2dec7
Patrick Yeon <patrickyeon@gmail.com>
2018-11-10 20:22:08 -0800

    remove redundant gpio setup

--- fw/src/gpio.c
+++ fw/src/gpio.c
@@ -11,10 +11,6 @@
 void gpio_init(void) {
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOB);
-    
-    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
-            (SET_ID_A | SET_ID_B | SET_IC_A | SET_IC_B));
-    gpio_clear(GPIOB, (SET_ID_A | SET_ID_B | SET_IC_A | SET_IC_B));
 
     gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, (DIR_A | CEN_B));
     gpio_clear(GPIOA, (DIR_A | CEN_B));
